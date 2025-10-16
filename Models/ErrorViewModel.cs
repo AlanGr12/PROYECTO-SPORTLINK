70abc478a8481671b8024406451ce7e223a948d6
ALAN GABRIEL GORDON <48860730@ort>
2025-10-16 14:50:16 -0300

    Alan gordon

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace proyectoSportlink.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
