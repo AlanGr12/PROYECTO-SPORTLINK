4fce122bb031e95ccca35b7e2e3ab9db860f827b
AlanGr12 <alanfutbol58@gmail.com>
2025-10-23 13:43:27 -0300

    Add files via upload

--- Models/Jugador.cs
+++ Models/Jugador.cs
@@ -1,4 +1,31 @@
 public class Jugador{
+public int idJugador{get;private set;}
+public string Nombre{get;private set;}
+public string Apellido{get;private set;}
+public string Telefono{get;private set;}
+public DateTime fechaNacimiento{get;private set;}
+public string fotoPerfil{get;private set;}
+public string Usuario{get;private set;}
+public string Password{get;private set;}
+public string Ubicacion{get;private set;}
+public string Genero{get;private set;}
+
+public Jugador(int idjugador,string nombre,string apellido,string telefono,DateTime fechanacimiento,string fotoperfil,string usuario,string password,string ubicacion,string genero)
+{
+this.idJugador= idjugador;
+this.Nombre = nombre;
+this.Apellido = apellido;
+this.Telefono = telefono;
+this.fechaNacimiento = fechanacimiento;
+this.fotoPerfil = fotoperfil;
+this.Usuario = usuario;
+this.Password = password;
+this.Ubicacion = ubicacion;
+this.Genero = genero;
+
+
+
+}
 
 
 
--- Models/Scouter.cs
+++ Models/Scouter.cs
@@ -1,3 +1,29 @@
 public class Scouter{
-    
+public int idScout {get;private set;}
+public string Nombre{get;private set;}
+public string Apellido{get;private set;}
+public string Club{get;private set;}
+public string Telefono{get;private set;}
+public string fotoPerfil{get;private set;}
+public string Usuario{get;private set;}
+public string Password{get;private set;}
+public string Email{get;private set;}
+
+public Scouter(int idscout,string nombre,string apellido,string club,string telefono,string fotoperfil,string usuario,string password,string email)
+{
+this.idScout = idscout;
+this.Nombre = nombre;
+this.Apellido = apellido;
+this.Club = club;
+this.Telefono = telefono;
+this.fotoPerfil = fotoperfil;
+this.Usuario = usuario;
+this.Password = password;
+this.Email = email;
+
+
+
+}
+
+
 }
